c324c628a6337a3fe35134c975d23b7f86e2c628
Onesmartshark <onesmartshark@gmail.com>
2024-10-22 20:13:35 -0400

    change choice layer

--- js/layers.js
+++ js/layers.js
@@ -755,14 +755,14 @@ addLayer("ch", {
         
     }},
     color: "yellow",
-    requires: new Decimal("1e1e1e1e100000"), // Can be a function that takes requirement increases into account
+    requires: new Decimal("1"), // Can be a function that takes requirement increases into account
     resource: "choices", // Name of prestige currency
     baseResource: "grass", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
 
     
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1, // Prestige currency exponent
+    exponent: 10000000000000000000, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
