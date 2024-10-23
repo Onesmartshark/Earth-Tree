12a95668016d410a18d54829d30b5bf3797b8c4e
Onesmartshark <onesmartshark@gmail.com>
2024-10-22 20:11:47 -0400

    Add choice upgrade

--- js/layers.js
+++ js/layers.js
@@ -675,12 +675,6 @@ addLayer("i", {
             cost: new Decimal(3),
             unlocked() { return hasUpgrade("i", 11)}, 
         },
-        13: {
-            title: "Metal Sprinkler",
-            description: "Quadruple grass, double trees (SOON).",
-            cost: new Decimal(10),
-            unlocked() { return hasUpgrade("i", 12)}, 
-        },
     },
     challenges: {
         11: {
@@ -712,7 +706,7 @@ addLayer("i", {
         },
         12: {
             name: "Extraless",
-            challengeDescription: "Effect of starting, but also lose access to clay and trees.",
+            challengeDescription: "Effect of starting, but also lose access to clay and trees. Unlock the first layer choice (hint: check below achivements).",
             goalDescription: "20 Coal",
             canComplete: function() {return player.co.points.gte(20)},
             rewardDescription: "Triple clay & trees.",
@@ -737,7 +731,62 @@ addLayer("i", {
                 player.g.milestones = []; 
             },
         },
+        13: {
+            name: "Unobtainable",
+            challengeDescription: "soon",
+            goalDescription: "1e48 Stone",
+            canComplete: function() {return player.s.points.gte(1e48)},
+            rewardDescription: "nothing.",
+            unlocked() { return hasChallenge('i', 12)}, 
+            onEnter() { 
+                player.s.points = new Decimal("-1e48"); 
+                player.s.upgrades = [];
+            },
+        },
     }
+})
+addLayer("ch", {
+    name: "choice", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+        
+    }},
+    color: "yellow",
+    requires: new Decimal("1e1e1e1e100000"), // Can be a function that takes requirement increases into account
+    resource: "choices", // Name of prestige currency
+    baseResource: "grass", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+
+    
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: "side", // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return hasChallenge('i', 12)},
+
+    upgrades: {
+        11: {
+            title: "Pick steel",
+            description: "Unlock steel for a longer grind, but extra bonuses.",
+            cost: new Decimal(0),
+            unlocked() { return hasChallenge("i", 12) && !hasUpgrade("ch", 12)}, 
+        },
+        12: {
+            title: "Pick gold",
+            description: "Skip steel, but lose the bonuses PERMANENTLY.",
+            cost: new Decimal(0),
+            unlocked() { return hasChallenge("i", 12) && !hasUpgrade("ch", 11)}, 
+        },
+    },
 })
 addLayer("a", {
     startData() { return {
