0d782dd1b98a2afbc2e7c020927add64d7a22313
Onesmartshark <onesmartshark@gmail.com>
2024-10-25 19:37:58 -0400

    get rid of compost its broken and annoying as heck

--- js/layers.js
+++ js/layers.js
@@ -905,67 +905,8 @@ addLayer("f", {
         },
         14: {
             title: "Compressed Dirt",
-            description: "Unlock compost.",
+            description: "Unlock compost (soon, might not be added, I tried 4 times to add it and it broke every time).",
             cost: new Decimal(10),
-        },
-    },
-})
-addLayer("cm", {
-    name: "compost", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "Cm", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: false,
-		points: new Decimal(0),
-        
-    }},
-    color: "brown",
-    requires: new Decimal(100), // Can be a function that takes requirement increases into account
-    resource: "compost", // Name of prestige currency
-    baseResource: "fruits", // Name of resource prestige is based on
-    baseAmount() {return player.f.points}, // Get the current amount of baseResource
-
-    
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        if (hasUpgrade('te', 23)) mult = mult.times(1000)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 4, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "f", description: "F: Reset for Fruits", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    doReset(resettingLayer) {
-        player.sl.milestones = []; 
-        if (layers[resettingLayer].row > this.row) layerDataReset("cm", keep)
-    },
-    layerShown(){return hasChallenge('i', 22)},
-
-    upgrades: {
-        11: {
-            title: "not gonna upd text",
-            description: "this is the 4th time I have tried to make this, figure out the actual boosts yourself",
-            cost: new Decimal(1),
-        },
-        12: {
-            title: "Bigger Growth",
-            description: "Triple grass.",
-            cost: new Decimal(2),
-        },
-        13: {
-            title: "Rooted Dirt",
-            description: "Double dirt.",
-            cost: new Decimal(3),
-        },
-        14: {
-            title: "Compressed Dirt",
-            description: "Unlock compost.",
-            cost: new Decimal(5),
         },
     },
 })
